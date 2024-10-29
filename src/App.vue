6d9eb6686028edaa6a38e04a36b4184d4573447f
minh03112k <hoangminh03112k@gmail.com>
2024-10-30 01:14:27 +0700

    first page

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
 <script setup lang="ts">
-import Button from './components/common/Button.vue';
-import { Icons } from './components/icons';
+  import Button from "./components/common/Button.vue";
+  import { Icons } from "./components/icons";
 </script>
 
 <template>
   <div>
     <div class="flex min-h-svh items-center justify-center flex-col space-y-12 px-5">
-      <img src="./assets/shoppe_logo.svg" alt="shoppe_logo" />
-      <h1 class="text-5xl font-bold">
-        Shoppe
-      </h1>
-      <p class="font-thin text-center text-lg">Beautiful eCommerce UI Kit <br /> for your online store</p>
-      <Button xl>
-        Let's get started!
-        <Icons.Arrow />
-      </Button>
-
-      <p>I already have an account
+      <div class="flex items-center justify-center shadow-[0_4px_8px_0_rgba(0,0,0,0.16)] p-8 rounded-full">
+        <img
+          src="./assets/shoppe_logo.svg"
+          alt="shoppe_logo"
+        />
+      </div>
+      <h1 class="text-5xl font-bold">Shoppe</h1>
+      <p class="font-thin text-center text-lg">
+        Beautiful eCommerce UI Kit <br />
+        for your online store
       </p>
+      <Button xl> Let's get started! </Button>
 
+      <span class="inline-flex items-center gap-4">
+        I already have an account
+        <Button sm> <Icons.Arrow /> </Button>
+      </span>
     </div>
   </div>
 </template>
--- src/components/common/Button.vue
+++ src/components/common/Button.vue
@@ -1,22 +1,25 @@
 <template>
-    <button class="rounded-2xl bg-blue-600 py-4 px-11 text-white 
-        transition-all duration-300
-        hover:brightness-90
-        active:translate-y-0.5 active:scale-[0.98] active:brightness-75
-        focus:outline-none 
-        " :class="{ 'w-full': xl, 'cursor-not-allowed opacity-50': disabled }">
-        <slot></slot>
-    </button>
+  <button
+    class="inline-flex items-center gap-4 justify-center rounded-2xl bg-blue-600 p-4 text-white transition-all duration-200 hover:brightness-90 active:translate-y-0.5 active:scale-[0.98] active:brightness-75 focus:outline-none"
+    :disabled="disabled"
+    :class="{ 'w-full': xl, 'w-fit rounded-full p-2': sm, 'cursor-not-allowed opacity-50': disabled }"
+  >
+    <div class="inline-flex items-center gap-4">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script setup lang="ts">
-defineProps({
+  defineProps({
     xl: {
-        type: Boolean
+      type: Boolean,
     },
     disabled: {
-        type: Boolean
-    }
-
-})
+      type: Boolean,
+    },
+    sm: {
+      type: Boolean,
+    },
+  });
 </script>
